74e0e7da8da744578c70499c1334f5d4762a4b2c
Greg-Bondb <91244314+Greg-Bondb@users.noreply.github.com>
2022-10-25 21:08:06 +0200

    add coins and menu restart

--- JetPack-Joyride/Form1.Designer.cs
+++ JetPack-Joyride/Form1.Designer.cs
@@ -36,18 +36,31 @@
             this.zapper1 = new System.Windows.Forms.PictureBox();
             this.zapper2 = new System.Windows.Forms.PictureBox();
             this.zapper3 = new System.Windows.Forms.PictureBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.Game_menu = new System.Windows.Forms.GroupBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.Score = new System.Windows.Forms.Label();
+            this.HighValue = new System.Windows.Forms.Label();
+            this.HighScore = new System.Windows.Forms.Label();
+            this.coin1 = new System.Windows.Forms.PictureBox();
+            this.Coin = new System.Windows.Forms.Label();
+            this.coin2 = new System.Windows.Forms.PictureBox();
             ((System.ComponentModel.ISupportInitialize)(this.gameArea)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.ground)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.caracter)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.zapper1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.zapper2)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.zapper3)).BeginInit();
+            this.Game_menu.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.coin1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.coin2)).BeginInit();
             this.SuspendLayout();
             // 
             // gameArea
             // 
             this.gameArea.BackColor = System.Drawing.Color.Transparent;
-            this.gameArea.Location = new System.Drawing.Point(2, 3);
+            this.gameArea.Location = new System.Drawing.Point(1, 2);
             this.gameArea.Name = "gameArea";
             this.gameArea.Size = new System.Drawing.Size(798, 448);
             this.gameArea.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
@@ -67,7 +80,7 @@
             // 
             this.caracter.BackColor = System.Drawing.Color.Transparent;
             this.caracter.Image = global::JetPack_Joyride.Properties.Resources.caracter;
-            this.caracter.Location = new System.Drawing.Point(103, 367);
+            this.caracter.Location = new System.Drawing.Point(100, 367);
             this.caracter.Name = "caracter";
             this.caracter.Size = new System.Drawing.Size(65, 63);
             this.caracter.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
@@ -116,6 +129,127 @@
             this.zapper3.TabStop = false;
             this.zapper3.Tag = "zapper";
             // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Segoe UI", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label3.ForeColor = System.Drawing.Color.Crimson;
+            this.label3.Location = new System.Drawing.Point(45, 23);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(205, 46);
+            this.label3.TabIndex = 17;
+            this.label3.Text = "GAME OVER";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label1.ForeColor = System.Drawing.Color.DarkSlateGray;
+            this.label1.Location = new System.Drawing.Point(93, 69);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(93, 37);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Restart";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // Game_menu
+            // 
+            this.Game_menu.Controls.Add(this.label3);
+            this.Game_menu.Controls.Add(this.label2);
+            this.Game_menu.Controls.Add(this.label1);
+            this.Game_menu.Location = new System.Drawing.Point(256, 140);
+            this.Game_menu.Name = "Game_menu";
+            this.Game_menu.Size = new System.Drawing.Size(289, 170);
+            this.Game_menu.TabIndex = 17;
+            this.Game_menu.TabStop = false;
+            this.Game_menu.Text = "Menu";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label2.ForeColor = System.Drawing.Color.DarkSlateGray;
+            this.label2.Location = new System.Drawing.Point(104, 112);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(64, 37);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Quit";
+            this.label2.Click += new System.EventHandler(this.label2_Click_1);
+            // 
+            // Score
+            // 
+            this.Score.AutoSize = true;
+            this.Score.BackColor = System.Drawing.Color.Cyan;
+            this.Score.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.Score.ForeColor = System.Drawing.Color.Black;
+            this.Score.Location = new System.Drawing.Point(577, 9);
+            this.Score.Name = "Score";
+            this.Score.Size = new System.Drawing.Size(75, 23);
+            this.Score.TabIndex = 21;
+            this.Score.Text = "Score : 0";
+            // 
+            // HighValue
+            // 
+            this.HighValue.AutoSize = true;
+            this.HighValue.BackColor = System.Drawing.Color.Cyan;
+            this.HighValue.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.HighValue.ForeColor = System.Drawing.Color.Black;
+            this.HighValue.Location = new System.Drawing.Point(763, 9);
+            this.HighValue.Name = "HighValue";
+            this.HighValue.Size = new System.Drawing.Size(19, 23);
+            this.HighValue.TabIndex = 23;
+            this.HighValue.Text = "0";
+            // 
+            // HighScore
+            // 
+            this.HighScore.AutoSize = true;
+            this.HighScore.BackColor = System.Drawing.Color.Cyan;
+            this.HighScore.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.HighScore.ForeColor = System.Drawing.Color.Black;
+            this.HighScore.Location = new System.Drawing.Point(668, 9);
+            this.HighScore.Name = "HighScore";
+            this.HighScore.Size = new System.Drawing.Size(102, 23);
+            this.HighScore.TabIndex = 22;
+            this.HighScore.Text = "High Score :";
+            // 
+            // coin1
+            // 
+            this.coin1.BackColor = System.Drawing.Color.Transparent;
+            this.coin1.Image = global::JetPack_Joyride.Properties.Resources.coin;
+            this.coin1.Location = new System.Drawing.Point(220, 430);
+            this.coin1.Name = "coin1";
+            this.coin1.Size = new System.Drawing.Size(40, 39);
+            this.coin1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.coin1.TabIndex = 24;
+            this.coin1.TabStop = false;
+            this.coin1.Tag = "coin";
+            // 
+            // Coin
+            // 
+            this.Coin.AutoSize = true;
+            this.Coin.BackColor = System.Drawing.Color.Cyan;
+            this.Coin.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.Coin.ForeColor = System.Drawing.Color.Black;
+            this.Coin.Location = new System.Drawing.Point(492, 9);
+            this.Coin.Name = "Coin";
+            this.Coin.Size = new System.Drawing.Size(68, 23);
+            this.Coin.TabIndex = 25;
+            this.Coin.Text = "Coin : 0";
+            // 
+            // coin2
+            // 
+            this.coin2.BackColor = System.Drawing.Color.Transparent;
+            this.coin2.Image = global::JetPack_Joyride.Properties.Resources.coin;
+            this.coin2.Location = new System.Drawing.Point(549, 430);
+            this.coin2.Name = "coin2";
+            this.coin2.Size = new System.Drawing.Size(40, 39);
+            this.coin2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.coin2.TabIndex = 29;
+            this.coin2.TabStop = false;
+            this.coin2.Tag = "coin";
+            // 
             // GameZone
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
@@ -123,16 +257,23 @@
             this.BackgroundImage = global::JetPack_Joyride.Properties.Resources.de4pasy_9dc3d7ad_2e4c_4930_9968_4bf756402c1d;
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.Coin);
             this.Controls.Add(this.ground);
+            this.Controls.Add(this.HighValue);
+            this.Controls.Add(this.HighScore);
+            this.Controls.Add(this.Score);
+            this.Controls.Add(this.Game_menu);
             this.Controls.Add(this.zapper3);
             this.Controls.Add(this.caracter);
             this.Controls.Add(this.zapper2);
             this.Controls.Add(this.zapper1);
+            this.Controls.Add(this.coin1);
+            this.Controls.Add(this.coin2);
             this.Controls.Add(this.gameArea);
             this.DoubleBuffered = true;
             this.MaximumSize = new System.Drawing.Size(818, 497);
             this.Name = "GameZone";
-            this.Text = "Form1";
+            this.Text = "Jetpack-Joyride";
             this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.KeyIsDown);
             ((System.ComponentModel.ISupportInitialize)(this.gameArea)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.ground)).EndInit();
@@ -140,7 +281,12 @@
             ((System.ComponentModel.ISupportInitialize)(this.zapper1)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.zapper2)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.zapper3)).EndInit();
+            this.Game_menu.ResumeLayout(false);
+            this.Game_menu.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.coin1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.coin2)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -153,5 +299,15 @@
         private PictureBox zapper1;
         private PictureBox zapper2;
         private PictureBox zapper3;
+        private Label label3;
+        private Label label1;
+        private GroupBox Game_menu;
+        private Label label2;
+        private Label Score;
+        private Label HighValue;
+        private Label HighScore;
+        private PictureBox coin1;
+        private Label Coin;
+        private PictureBox coin2;
     }
 }
--- JetPack-Joyride/Form1.cs
+++ JetPack-Joyride/Form1.cs
@@ -3,6 +3,7 @@ using static System.Formats.Asn1.AsnWriter;
 using System;
 using System.Diagnostics;
 using static System.Windows.Forms.VisualStyles.VisualStyleElement.Rebar;
+using System.Windows.Forms;
 
 namespace JetPack_Joyride
 {
@@ -11,8 +12,7 @@ namespace JetPack_Joyride
         // public var
         bool jump;
         int gravity = 14;
-        int force;
-        int randomzapper;
+        int force, score, randomzapper, compteur, score_coin;
         Random rand = new Random();
         System.Windows.Forms.PictureBox zappername;
 
@@ -23,6 +23,8 @@ namespace JetPack_Joyride
 
         private void mainTimer(object sender, EventArgs e)
         {
+
+            Game_menu.Hide();
 
             // jump
             if (jump)
@@ -45,6 +47,16 @@ namespace JetPack_Joyride
                 jump = false;
             }
 
+            // register the Best Score
+            int BestScore = Int32.Parse(HighValue.Text);
+
+            if (score > BestScore)
+            {
+                HighValue.Text = score.ToString();
+                Properties.Settings.Default.HScore = HighValue.Text;
+                Properties.Settings.Default.Save();
+            }
+
             // zapper show or zapper intersect with caracter
             foreach (Control x in this.Controls)
             {
@@ -56,12 +68,24 @@ namespace JetPack_Joyride
                     // game over
                     if (caracter.Bounds.IntersectsWith(x.Bounds))
                     {
-                        //stop
+                        timer.Stop();
+                        Game_menu.Show();
+                        score = 0;
+                        compteur = 0;
+                        score_coin = 0;
+                        jump = false;
                     }
 
                     // redefinde zapper compared the gameArea
                     if (x.Right < gameArea.Left)
                     {
+
+                        // add score
+                        if (compteur > 1)
+                        {
+                            score++;
+                            Score.Text = "Score :" + score;
+                        }
 
                         // take a random enemy skin
                         randomzapper = rand.Next(0, 3);
@@ -86,6 +110,32 @@ namespace JetPack_Joyride
                             case 3: zappername.Image = Properties.Resources.L12; zappername.Size = new Size(50, 140); break;
                         }
 
+                        compteur++;
+                        x.Location = new Point(gameArea.Right, rand.Next(gameArea.Top, (gameArea.Bottom - x.Height - ground.Height)));
+                    }
+                }
+            }
+
+            // coin
+            foreach (Control x in this.Controls)
+            {
+                if ((string)x.Tag == "coin")
+                {
+                    // coin left scroll
+                    x.Location = new Point(x.Location.X - 4, x.Location.Y);
+
+                    // add coin
+                    if (caracter.Bounds.IntersectsWith(x.Bounds)&& x.Visible == true)
+                    {
+                        x.Hide();
+                        score_coin++;
+                        Coin.Text = "Coin :" + score_coin;
+                    }
+
+                    // redefinde coin compared the gameArea
+                    if (x.Right < gameArea.Left)
+                    {
+                        x.Show();
                         x.Location = new Point(gameArea.Right, rand.Next(gameArea.Top, (gameArea.Bottom - x.Height - ground.Height)));
                     }
                 }
@@ -103,6 +153,27 @@ namespace JetPack_Joyride
             {
                 jump = false;
             }
+        }
+
+        // To make a clean restart at the game launch
+        private void label1_Click(object sender, EventArgs e)
+        {
+            timer.Start();
+            Game_menu.Hide();
+            Score.Text = "Score :" + score;
+            Coin.Text = "Coin :" + score_coin;
+            caracter.Location = new Point(100, 367);
+            zapper1.Location = new Point(2, 430);
+            zapper2.Location = new Point(326, 430);
+            zapper3.Location = new Point(649, 430);
+            coin1.Location = new Point(220, 430);
+            coin2.Location = new Point(549, 430);
+        }
+
+        // To quit the game
+        private void label2_Click_1(object sender, EventArgs e)
+        {
+            Application.Exit();
         }
     }
 }
